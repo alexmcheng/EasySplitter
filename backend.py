99e8ae54e558f970bbc8cca15bb6a84ad9a71ba8
Alex Cheng <cubesolver@gmail.com>
2018-06-13 00:25:22 -0700

    adding requirements file and making SQL query trigger on webpage visit

--- backend.py
+++ backend.py
@@ -1,4 +1,6 @@
 # EasySplitter by Alex Cheng
+
+# Using Flask framework and SQLAlchemy ORM for SQL querying.
 import login
 from flask import Flask
 from sqlalchemy import create_engine, Column, Integer, String, Date, ForeignKey
@@ -7,10 +9,9 @@ from sqlalchemy.orm import sessionmaker, relationship
 
 
 app = Flask(__name__)
-
 Base = declarative_base()
 
-# define our table columns
+# Defining table columns.
 class Transaction(Base):
     __tablename__ = 'user_spending'
 
@@ -29,43 +30,48 @@ class TransactionDetails(Base):
     notes = Column('notes', String(60))
     who_paid = Column('who_paid', Integer)
 
-# MySQL database login details, username, password, addresss
-connection_string: str = 'mysql+pymysql://' + login.database_str
+# MySQL database login details, username, password, and address from login.py.
+connection_string = 'mysql+pymysql://' + login.database_str
 
+# Connect to database via engine creation.
 engine = create_engine(connection_string, echo=True)
 Base.metadata.create_all(bind=engine)
 
 Session = sessionmaker(engine)
 session = Session()
 
+# Creating detail object
 detail = TransactionDetails()
-detail.restaurant = 'Seto'
-detail.date = '2018-06-11'
+detail.restaurant = 'Joy Wok'
+detail.date = '2018-06-12'
 detail.notes = 'delicious'
 detail.who_paid = 0
 session.add(detail)
+session.flush()
 
+# Creating user 0 spending object
 trans0 = Transaction()
 trans0.spender_id = 0
-trans0.amt_spent = 1800
+trans0.amt_spent = 1000
 trans0.detail_id = detail.id
 
+# Creating user 1 spending object
 trans1 = Transaction()
 trans1.spender_id = 1
-trans1.amt_spent = 900
+trans1.amt_spent = 1000
 trans1.detail_id = detail.id
-
 
 session.add(trans0)
 session.add(trans1)
 
 session.commit()
-
 session.close()
 
+# Creating default Flask route
 @app.route('/')
 def index():
-    return "Hello World!!! Yolo"
+    return "Home Page"
 
+# Starts the Flask app
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
